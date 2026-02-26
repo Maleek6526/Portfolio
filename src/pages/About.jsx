0230a7e30180dfbe568b5d63950db6f9908c7ee4
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2026-02-26 21:24:26 +0100

    profile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
-import profileImg from "../assets/IMG-20251018-WA0189.jpg";
+// import profileImg from "../assets/IMG-20251018-WA0189.jpg";
+import profileImg from "../assets/IMG_1108.jpeg";
 
 export default function About() {
   return (
